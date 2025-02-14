79e3c712ac5dbec4e39817ae3b5aa26c138b9bc6
Ray Zane <raymondzane@gmail.com>
2025-02-13 19:52:41 -0500

    Disable GC

--- benchmark.rb
+++ benchmark.rb
@@ -24,9 +24,34 @@ strategies = {
   }
 }
 
+# See: https://github.com/evanphx/benchmark-ips?tab=readme-ov-file#custom-suite
+class SuiteWithoutGC
+  def warming(*)
+    run_gc
+  end
+
+  def running(*)
+    run_gc
+  end
+
+  def warmup_stats(*)
+  end
+
+  def add_report(*)
+  end
+
+  private
+
+  def run_gc
+    GC.enable
+    GC.start
+    GC.disable
+  end
+end
+
 puts "== Cold cache =="
 Benchmark.ips do |x|
-  x.config(warmup: 2, time: 5)
+  x.config(warmup: 2, time: 5, suite: SuiteWithoutGC.new)
   strategies.each do |name, klass|
     x.report(name) { klass.new.value }
   end
@@ -35,7 +60,7 @@ end
 
 puts "== Warm cache =="
 Benchmark.ips do |x|
-  x.config(warmup: 2, time: 5)
+  x.config(warmup: 2, time: 5, suite: SuiteWithoutGC.new)
   strategies.each do |name, klass|
     instance = klass.new
     instance.value
