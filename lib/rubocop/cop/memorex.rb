0bb904e96bea99cb0b338e66533c77e29981ca5a
Ray Zane <raymondzane@gmail.com>
2025-02-14 11:51:00 -0500

    Document cops

--- lib/rubocop/cop/memorex.rb
+++ lib/rubocop/cop/memorex.rb
@@ -5,25 +5,46 @@ require "rubocop"
 module RuboCop
   module Cop
     module Memorex
+      # This cop checks for memoized methods that accept arguments or yield.
+      #
+      # @example
+      #   # bad
+      #   def foo; end
+      #   memoize :foo
+      #
+      #   # good
+      #   memoize def foo; end
       class MethodDefinition < RuboCop::Cop::Base
+        # @return [String]
         MSG = "`memoize` should precede a method definition (e.g. `memoize def`)"
 
         def_node_matcher :invalid_usage?, <<~PATTERN
           (send nil? :memoize (sym _))
         PATTERN
 
+        # @!visibility private
         def on_send(node)
           add_offense(node.selector) if invalid_usage?(node)
         end
       end
 
+      # This cop checks for memoized methods that accept arguments or yield.
+      #
+      # @example
+      #   # bad
+      #   memoize def foo(a); end
+      #
+      #   # good
+      #   memoize def foo; end
       class MethodSignature < RuboCop::Cop::Base
+        # @return [String]
         MSG = "Memoized methods should not accept arguments or yield"
 
         def_node_matcher :invalid_signature?, <<~PATTERN
           (send nil? :memoize {(def _name (args _+) _body) | (def _name _args `yield)})
         PATTERN
 
+        # @!visibility private
         def on_send(node)
           add_offense(node.selector) if invalid_signature?(node)
         end
